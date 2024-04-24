d933b18c8fc187f3c077ff67f9d3d9a2b2174fda
1704mori <1704mori@gmail.com>
2024-04-24 12:26:42 -0300

    update api.ts to include default url and headers

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,4 +1,30 @@
+import { PUBLIC_API_TOKEN, PUBLIC_API_URL } from "$env/static/public";
+
 export async function api(request: RequestInfo, init?: RequestInit) {
+    const baseUrl = PUBLIC_API_URL || "__API_URL__";
+    const token = PUBLIC_API_TOKEN || "__API_TOKEN__";
+
+    if (typeof request === 'string') {
+        request = `${baseUrl}/api/v1${request}`;
+    }
+
+    if (init?.headers && !('Authorization' in init.headers)) {
+        init.headers = {
+            ...init.headers,
+            Authorization: token,
+        };
+    }
+
+    if (!init?.headers) {
+        init = {
+            ...init,
+            headers: {
+                Authorization: token,
+                'Content-Type': 'application/json',
+            },
+        };
+    }
+
     const res = await fetch(request, init);
     if (!res.ok) {
         const response = await res.json();

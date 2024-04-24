05314f1f0a8f942b66bcdf9965ffa9e4d811825e
1704mori <1704mori@gmail.com>
2024-04-24 13:25:37 -0300

    Update ServeJSON middleware to skip non-API routes

--- api/middleware/json.go
+++ api/middleware/json.go
@@ -1,9 +1,17 @@
 package middleware
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+)
 
 func ServeJSON(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !strings.HasPrefix(r.URL.Path, "/api/v1/") {
+			next.ServeHTTP(w, r)
+			return
+		}
+
 		w.Header().Set("Content-Type", "application/json")
 		next.ServeHTTP(w, r)
 	})

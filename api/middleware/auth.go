5ee60bc87d5104686027b11bb82c5f1ef6c91c19
1704mori <1704mori@gmail.com>
2024-04-24 13:12:28 -0300

    Update auth middleware to use strings.HasPrefix instead of slicing URL path

--- api/middleware/auth.go
+++ api/middleware/auth.go
@@ -2,13 +2,14 @@ package middleware
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/1704mori/kinko/internal/config"
 )
 
 func Auth(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path[:8] != "/api/v1/" {
+		if !strings.HasPrefix(r.URL.Path, "/api/v1/") {
 			next.ServeHTTP(w, r)
 			return
 		}

dd2154cfe5060edfd6e1da4456c1e839edf93f60
1704mori <1704mori@gmail.com>
2024-04-24 13:03:31 -0300

    improve form.ts with error messages

--- frontend/src/lib/form.ts
+++ frontend/src/lib/form.ts
@@ -22,7 +22,14 @@ export function submit<T = any>(handler: (data: T) => void): (event: SubmitEvent
 						continue;
 					}
 
+					if (input.getAttribute('required') && !value) {
+						valid = false;
+					}
+
 					payload[input.getAttribute('name')!] = value;
+				} else {
+					valid = false;
+					validateForm(input as HTMLElement, true);
 				}
 			} else {
 				if (import.meta.env.MODE != 'test') {
@@ -50,20 +57,28 @@ export function submit<T = any>(handler: (data: T) => void): (event: SubmitEvent
 	};
 }
 
-// finalizar validador caso venhamos a usar
-// labels em cima dos inputs para mostrar errors
-// e ajustar validator para aceitar Select
-function validateForm(node: HTMLElement): { destroy: () => void } {
-	const output = node.offsetParent?.previousElementSibling;
+function validateForm(node: HTMLElement, isSelectInvalid?: boolean): { destroy: () => void } {
+	// @ts-ignore
+	let output = node.tagName === "BUTTON" && node.classList.contains("select") ?  node.parentNode?.previousElementSibling : node.parentNode;
+
+	const errorSpan = getErrorSpan(output!);
+	console.log(errorSpan);
 
 	node.addEventListener('invalid', onInvalid);
 	node.addEventListener('input', onInput);
-
-	function onInvalid() {
-		// ...
+	if (isSelectInvalid) {
+		onInvalid();
 	}
 
-	function onInput() {}
+	function onInvalid() {
+		// @ts-ignore
+		errorSpan.textContent = node.tagName === 'BUTTON' && node.classList.contains('select') ? 'Selecione uma opção' : node.validationMessage;
+	}
+
+	function onInput() {
+		// @ts-ignore
+		if (node.validationMessage == '') errorSpan.textContent = '';
+	}
 
 	return {
 		destroy() {
@@ -71,4 +86,48 @@ function validateForm(node: HTMLElement): { destroy: () => void } {
 			node.removeEventListener('input', onInput);
 		}
 	};
+}
+
+function getErrorSpan(node: HTMLElement) {
+	// @ts-ignore
+	if (node.firstChild?.firstElementChild?.nodeName === 'SPAN') {
+		// @ts-ignore
+		return node.firstChild.firstElementChild;
+	}
+
+	console.log(node);
+	// @ts-ignore
+	if (node.lastChild.nodeName === 'SPAN') {
+		return node.lastChild;
+	}
+
+	// @ts-ignore
+	if (node.parentNode.previousElementSibling.lastChild.nodeName === 'SPAN') {
+		// @ts-ignore
+		return node.parentNode.previousElementSibling.lastChild;
+	}
+}
+
+function getCustomErrorMessage(node: HTMLInputElement, t: any) {
+	if (node.validity.valueMissing) {
+		return t.get('auth.errors.valueMissing');
+	} else if (node.validity.typeMismatch) {
+		return t.get('auth.errors.typeMismatch');
+	} else if (node.validity.tooShort) {
+		return t.get('auth.errors.tooShort', { minLength: node.minLength });
+	} else if (node.validity.tooLong) {
+		return t.get('auth.errors.tooLong', { maxLength: node.maxLength });
+	} else if (node.validity.patternMismatch) {
+		return t.get('auth.errors.patternMismatch');
+	} else if (node.validity.rangeOverflow) {
+		return t.get('auth.errors.rangeOverflow', { max: node.max });
+	} else if (node.validity.rangeUnderflow) {
+		return t.get('auth.errors.rangeUnderflow', { min: node.min });
+	} else if (node.validity.stepMismatch) {
+		return t.get('auth.errors.stepMismatch');
+	} else if (node.validity.badInput) {
+		return t.get('auth.errors.badInput');
+	}
+
+	return '';
 }
